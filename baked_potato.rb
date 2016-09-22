fdf4a6b112b1a74111a4dd271568af266f9f5a65
voodoologic <headley.douglas@gmail.com>
2016-09-22 10:06:16 -0700

    additions to webpage

--- baked_potato.rb
+++ baked_potato.rb
@@ -7,14 +7,18 @@ class BakedPotato < Sinatra::Base
   set env: :development
   set port: 4545
   set :bind, '0.0.0.0'
-  @cache_folder = '/Users/voodoologic/Sandbox/cache_folder'
   @box_client = Utils::Box::Client.instance
   @sf_client  = Utils::SalesForce::Client.instance
+
+  image_path = Pathname.new('./public') + Date.today.to_s
+  FileUtils.ln_s( CacheFolder.path, image_path ) unless image_path.exist?
+
   get '/' do
     @image       = BPImage.random_unlocked
     @image.lock
-    @full_path   = @image.full_path.split('/')[-2..-1].join('/')
-    @name        = @image.full_path.split('/')[-1]
+    @full_path   = get_full_path
+    @name        = @image.path.basename
+    binding.pry
     @size        = @image.db_image.size
     @type        = @image.db_image.type
     @fingerprint = @image.db_image.fingerprint
@@ -30,6 +34,12 @@ class BakedPotato < Sinatra::Base
     # cf   = CacheFolder.new(opp_id: :opportunity_id)
     # @images = cf.images.each{|i| i.lock}
     # @opp = @sf_client.custom_query(query: query)
+  end
+
+
+  def get_full_path
+    path_array = @image.path.parent.to_s.split('/')[5..-1] << @image.path.basename.to_s
+    path_array.join('/')
   end
 
   run! if app_file == $0
