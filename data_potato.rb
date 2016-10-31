4c1926fc7c435104aa62ed6b4f758aa6d4f61c0f
voodoologic <headley.douglas@gmail.com>
2016-10-31 22:52:17 +0000

    data_potato

--- data_potato.rb
+++ data_potato.rb
@@ -13,6 +13,92 @@ class DataPotato
     DB::ImageProgressRecord.create_new_from_path(path) if path
   end
 
+  def remove_meta_yml
+    Dir.glob('/home/doug/Sandbox/dated_cache_folder/2016-10-31/**/*').each do |entity|
+      path = Pathname.new(entity)
+      puts path
+      FileUtils.rm(path) if path.basename == 'meta.yml'
+    end
+  end
+
+  def name_case_attribution
+    #this will remove the need for meta.yml
+    @sf_client = Utils::SalesForce::Client.new
+    DB::SalesForceProgressRecord.all(object_type: 'Case').each_slice(50).with_index do |records, i|
+      begin
+        puts i
+        ids = records.map(&:sales_force_id)
+        # id = record.sales_force_id
+        @sf_client.custom_query(query: "SELECT id, casenumber, subject FROM Case where id in #{ids.to_s.gsub('[','(').gsub(']',')').gsub('"', "'")}")
+      rescue => e
+        ap e.backtrace
+        puts e
+        binding.pry
+      end
+    end
+  end
+
+  def name_opp_attribution
+    #this will remove the need for meta.yml
+    @sf_client = Utils::SalesForce::Client.new
+    DB::SalesForceProgressRecord.all(object_type: 'Opportunity').each_slice(50).with_index do |records, i|
+      begin
+        puts i
+        ids = records.map(&:sales_force_id)
+        # id = record.sales_force_id
+        @sf_client.custom_query(query: "SELECT id, name FROM Opportunity where id in #{ids.to_s.gsub('[','(').gsub(']',')').gsub('"', "'")}")
+      rescue => e
+        ap e.backtrace
+        puts e
+        binding.pry
+      end
+    end
+  end
+
+  def relative_path
+    binding.pry
+    DB::ImageProgressRecord.all.batch(1000).each_with_index do |record, i|
+      relative_path = record.full_path.to_s.split('/')[6..-1].join('/')
+      record.relative_path = relative_path
+      if i % 1000 == 0
+        puts i
+        puts relative_path
+      end
+      record.save
+    end
+  end
+
+  def glob_test
+    binding.pry
+    hello = Dir.glob('/home/doug/Sandbox/s_drive/Client Management/REED HEIN and ASSOCIATES/_Timeshare Exits/**/*')
+  end
+
+  def local_migration
+    DB::SMBRecord.all.batch(1000).each_with_index do |record, i|
+      path = record.path
+      relative_path = path.split('/')[4..-1].join('/')
+      record.local_path = '/home/doug/Sandbox/s_drive/'
+      record.network_path = '/Client Management/REED HEIN and ASSOCIATES/_Timeshare Exits/'
+      record.relative_path = relative_path
+      super_path = record.local_path.chomp('/') + record.network_path + record.relative_path
+      record_path =  Pathname.new(super_path)
+      if record_path.exist?
+        record_path.file? ? record.type = :file : record.type = :directory
+        record.save
+      else
+        record.destroy
+      end
+      puts i if i % 100 == 0
+    end
+  end
+
+  def add_date_to_smb_records
+    DB::SMBRecord.all.batch(1000).each_with_index do |record,i|
+      puts i if i % 1000 == 0
+      record.update(date: Date.today.to_s) if record.date.nil?
+    end
+  end
+
   def delete_dumb_records
     DB::SMBRecord.all.batch(1000).each_with_index do |record, i|
       puts i
@@ -79,7 +165,7 @@ end
 
 begin
   dp =  DataPotato.new
-  dp.delete_dumb_records
+  dp.remove_meta_yml
   # dp.destroy_all
   # files = dp.get_files_from_cache_folder
   # records = dp.create_db_for_file_strings(files)
