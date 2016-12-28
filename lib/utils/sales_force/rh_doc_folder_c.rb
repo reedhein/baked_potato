f5f700cc3c4a0b4b12a52e1b8bec21d79d83616b
voodoologic <headley.douglas@gmail.com>
2016-12-28 10:06:40 -0800

    new objects

---
+++ lib/utils/sales_force/rh_doc_folder_c.rb
@@ -0,0 +1,7 @@
+module Utils
+  module SalesForce
+    class RHDocFolderC < Utils::SalesForce::Base
+      attr_accessor :id, :case__c
+    end
+  end
+end
---
+++ lib/utils/sales_force/ts_doc_folder_c.rb
@@ -0,0 +1,7 @@
+module Utils
+  module SalesForce
+    class TSDocFolderC < Utils::SalesForce::Base
+      attr_accessor :id, :case__c
+    end
+  end
+end
